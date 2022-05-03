8af00561ed5c2848b66c7ca9a17afd4b645d5408
Tbread <81889764+Tbread@users.noreply.github.com>
2022-05-03 18:08:31 +0900

    Feat: Gym 모델추가

---
+++ src/main/kotlin/com/healthcare/managingpt/model/Gym.kt
@@ -0,0 +1,31 @@
+package com.healthcare.managingpt.model
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+
+
+@Entity
+class Gym :TimeStamped(){
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id:Long = 0
+
+    @Column(nullable = false)
+    var name:String = ""
+
+    @Column(nullable = false)
+    var tel:String = ""
+
+    @Column(nullable = false)
+    var address:String = ""
+
+    @ManyToOne
+    @JoinColumn
+    var owner:User? = null
+}
